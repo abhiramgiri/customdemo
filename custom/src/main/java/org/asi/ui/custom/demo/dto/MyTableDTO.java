6b5efb91d29ad6e66846c5111696fd3c15173b1f
abhiramgiri <abhiram.giri@gmail.com>
2015-01-14 14:25:44 +0530

    Create MyTableDTO.java

---
+++ custom/src/main/java/org/asi/ui/custom/demo/dto/MyTableDTO.java
@@ -0,0 +1,474 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.asi.ui.custom.demo.dto;
+
+/**
+ *
+ * @author Abhiram
+ */
+public class MyTableDTO {
+    private int projectionType;
+    private String allocationType = "Hi";
+    private String frequency = "Hi";
+    private String projCol1Sales = "hello";
+    private String projCol2Sales = "hello";
+    private String projCol3Sales = "hello";
+    private String projCol4Sales = "hello";
+    private String projCol5Sales = "hello";
+    private String projCol6Sales = "hello";
+    private String projCol7Sales = "hello";
+    private String projCol8Sales = "hello";
+    private String projCol9Sales = "hello";
+    private String projCol10Sales = "hello";
+    private String projCol11Sales = "hello";
+    private String projCol12Sales = "hello";
+    private String projCol13Sales = "hello";
+    private String projCol14Sales = "hello";
+    private String projCol15Sales = "hello";
+    private String projCol16Sales = "hello";
+    private String projCol17Sales = "hello";
+    private String projCol18Sales = "hello";
+    private String projCol19Sales = "hello";
+    private String projCol20Sales = "hello";
+    private String projCol21Sales = "hello";
+    private String projCol22Sales = "hello";
+    private String projCol23Sales = "hello";
+    private String projCol24Sales = "hello";
+    private String projCol25Sales = "hello";
+    private String projCol26Sales = "hello";
+    private String projCol27Sales = "hello";
+    private String projCol28Sales = "hello";
+    private String projCol29Sales = "hello";
+    private String projCol30Sales = "hello";
+    private String projCol31Sales = "hello";
+    private String projCol32Sales = "hello";
+    private String projCol33Sales = "hello";
+    private String projCol34Sales = "hello";
+    private String projCol35Sales = "hello";
+    private String projCol36Sales = "hello";
+    private String projCol37Sales = "hello";
+    private String projCol38Sales = "hello";
+    private String projCol39Sales = "hello";
+    private String projCol40Sales = "hello";
+    private String projCol41Sales = "hello";
+    private String projCol42Sales = "hello";
+    private String projCol43Sales = "hello";
+    private String projCol44Sales = "hello";
+    private String projCol45Sales = "hello";
+    private String projCol46Sales = "hello";
+    private String projCol47Sales = "hello";
+    private String projCol48Sales = "hello";
+
+    public int getProjectionType() {
+        return projectionType;
+    }
+
+    public void setProjectionType(int projectionType) {
+        this.projectionType = projectionType;
+    }
+
+    public String getAllocationType() {
+        return allocationType;
+    }
+
+    public void setAllocationType(String allocationType) {
+        this.allocationType = allocationType;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getProjCol1Sales() {
+        return projCol1Sales;
+    }
+
+    public void setProjCol1Sales(String projCol1Sales) {
+        this.projCol1Sales = projCol1Sales;
+    }
+
+    public String getProjCol2Sales() {
+        return projCol2Sales;
+    }
+
+    public void setProjCol2Sales(String projCol2Sales) {
+        this.projCol2Sales = projCol2Sales;
+    }
+
+    public String getProjCol3Sales() {
+        return projCol3Sales;
+    }
+
+    public void setProjCol3Sales(String projCol3Sales) {
+        this.projCol3Sales = projCol3Sales;
+    }
+
+    public String getProjCol4Sales() {
+        return projCol4Sales;
+    }
+
+    public void setProjCol4Sales(String projCol4Sales) {
+        this.projCol4Sales = projCol4Sales;
+    }
+
+    public String getProjCol5Sales() {
+        return projCol5Sales;
+    }
+
+    public void setProjCol5Sales(String projCol5Sales) {
+        this.projCol5Sales = projCol5Sales;
+    }
+
+    public String getProjCol6Sales() {
+        return projCol6Sales;
+    }
+
+    public void setProjCol6Sales(String projCol6Sales) {
+        this.projCol6Sales = projCol6Sales;
+    }
+
+    public String getProjCol7Sales() {
+        return projCol7Sales;
+    }
+
+    public void setProjCol7Sales(String projCol7Sales) {
+        this.projCol7Sales = projCol7Sales;
+    }
+
+    public String getProjCol8Sales() {
+        return projCol8Sales;
+    }
+
+    public void setProjCol8Sales(String projCol8Sales) {
+        this.projCol8Sales = projCol8Sales;
+    }
+
+    public String getProjCol9Sales() {
+        return projCol9Sales;
+    }
+
+    public void setProjCol9Sales(String projCol9Sales) {
+        this.projCol9Sales = projCol9Sales;
+    }
+
+    public String getProjCol10Sales() {
+        return projCol10Sales;
+    }
+
+    public void setProjCol10Sales(String projCol10Sales) {
+        this.projCol10Sales = projCol10Sales;
+    }
+
+    public String getProjCol11Sales() {
+        return projCol11Sales;
+    }
+
+    public void setProjCol11Sales(String projCol11Sales) {
+        this.projCol11Sales = projCol11Sales;
+    }
+
+    public String getProjCol12Sales() {
+        return projCol12Sales;
+    }
+
+    public void setProjCol12Sales(String projCol12Sales) {
+        this.projCol12Sales = projCol12Sales;
+    }
+
+    public String getProjCol13Sales() {
+        return projCol13Sales;
+    }
+
+    public void setProjCol13Sales(String projCol13Sales) {
+        this.projCol13Sales = projCol13Sales;
+    }
+
+    public String getProjCol14Sales() {
+        return projCol14Sales;
+    }
+
+    public void setProjCol14Sales(String projCol14Sales) {
+        this.projCol14Sales = projCol14Sales;
+    }
+
+    public String getProjCol15Sales() {
+        return projCol15Sales;
+    }
+
+    public void setProjCol15Sales(String projCol15Sales) {
+        this.projCol15Sales = projCol15Sales;
+    }
+
+    public String getProjCol16Sales() {
+        return projCol16Sales;
+    }
+
+    public void setProjCol16Sales(String projCol16Sales) {
+        this.projCol16Sales = projCol16Sales;
+    }
+
+    public String getProjCol17Sales() {
+        return projCol17Sales;
+    }
+
+    public void setProjCol17Sales(String projCol17Sales) {
+        this.projCol17Sales = projCol17Sales;
+    }
+
+    public String getProjCol18Sales() {
+        return projCol18Sales;
+    }
+
+    public void setProjCol18Sales(String projCol18Sales) {
+        this.projCol18Sales = projCol18Sales;
+    }
+
+    public String getProjCol19Sales() {
+        return projCol19Sales;
+    }
+
+    public void setProjCol19Sales(String projCol19Sales) {
+        this.projCol19Sales = projCol19Sales;
+    }
+
+    public String getProjCol20Sales() {
+        return projCol20Sales;
+    }
+
+    public void setProjCol20Sales(String projCol20Sales) {
+        this.projCol20Sales = projCol20Sales;
+    }
+
+    public String getProjCol21Sales() {
+        return projCol21Sales;
+    }
+
+    public void setProjCol21Sales(String projCol21Sales) {
+        this.projCol21Sales = projCol21Sales;
+    }
+
+    public String getProjCol22Sales() {
+        return projCol22Sales;
+    }
+
+    public void setProjCol22Sales(String projCol22Sales) {
+        this.projCol22Sales = projCol22Sales;
+    }
+
+    public String getProjCol23Sales() {
+        return projCol23Sales;
+    }
+
+    public void setProjCol23Sales(String projCol23Sales) {
+        this.projCol23Sales = projCol23Sales;
+    }
+
+    public String getProjCol24Sales() {
+        return projCol24Sales;
+    }
+
+    public void setProjCol24Sales(String projCol24Sales) {
+        this.projCol24Sales = projCol24Sales;
+    }
+
+    public String getProjCol25Sales() {
+        return projCol25Sales;
+    }
+
+    public void setProjCol25Sales(String projCol25Sales) {
+        this.projCol25Sales = projCol25Sales;
+    }
+
+    public String getProjCol26Sales() {
+        return projCol26Sales;
+    }
+
+    public void setProjCol26Sales(String projCol26Sales) {
+        this.projCol26Sales = projCol26Sales;
+    }
+
+    public String getProjCol27Sales() {
+        return projCol27Sales;
+    }
+
+    public void setProjCol27Sales(String projCol27Sales) {
+        this.projCol27Sales = projCol27Sales;
+    }
+
+    public String getProjCol28Sales() {
+        return projCol28Sales;
+    }
+
+    public void setProjCol28Sales(String projCol28Sales) {
+        this.projCol28Sales = projCol28Sales;
+    }
+
+    public String getProjCol29Sales() {
+        return projCol29Sales;
+    }
+
+    public void setProjCol29Sales(String projCol29Sales) {
+        this.projCol29Sales = projCol29Sales;
+    }
+
+    public String getProjCol30Sales() {
+        return projCol30Sales;
+    }
+
+    public void setProjCol30Sales(String projCol30Sales) {
+        this.projCol30Sales = projCol30Sales;
+    }
+
+    public String getProjCol31Sales() {
+        return projCol31Sales;
+    }
+
+    public void setProjCol31Sales(String projCol31Sales) {
+        this.projCol31Sales = projCol31Sales;
+    }
+
+    public String getProjCol32Sales() {
+        return projCol32Sales;
+    }
+
+    public void setProjCol32Sales(String projCol32Sales) {
+        this.projCol32Sales = projCol32Sales;
+    }
+
+    public String getProjCol33Sales() {
+        return projCol33Sales;
+    }
+
+    public void setProjCol33Sales(String projCol33Sales) {
+        this.projCol33Sales = projCol33Sales;
+    }
+
+    public String getProjCol34Sales() {
+        return projCol34Sales;
+    }
+
+    public void setProjCol34Sales(String projCol34Sales) {
+        this.projCol34Sales = projCol34Sales;
+    }
+
+    public String getProjCol35Sales() {
+        return projCol35Sales;
+    }
+
+    public void setProjCol35Sales(String projCol35Sales) {
+        this.projCol35Sales = projCol35Sales;
+    }
+
+    public String getProjCol36Sales() {
+        return projCol36Sales;
+    }
+
+    public void setProjCol36Sales(String projCol36Sales) {
+        this.projCol36Sales = projCol36Sales;
+    }
+
+    public String getProjCol37Sales() {
+        return projCol37Sales;
+    }
+
+    public void setProjCol37Sales(String projCol37Sales) {
+        this.projCol37Sales = projCol37Sales;
+    }
+
+    public String getProjCol38Sales() {
+        return projCol38Sales;
+    }
+
+    public void setProjCol38Sales(String projCol38Sales) {
+        this.projCol38Sales = projCol38Sales;
+    }
+
+    public String getProjCol39Sales() {
+        return projCol39Sales;
+    }
+
+    public void setProjCol39Sales(String projCol39Sales) {
+        this.projCol39Sales = projCol39Sales;
+    }
+
+    public String getProjCol40Sales() {
+        return projCol40Sales;
+    }
+
+    public void setProjCol40Sales(String projCol40Sales) {
+        this.projCol40Sales = projCol40Sales;
+    }
+
+    public String getProjCol41Sales() {
+        return projCol41Sales;
+    }
+
+    public void setProjCol41Sales(String projCol41Sales) {
+        this.projCol41Sales = projCol41Sales;
+    }
+
+    public String getProjCol42Sales() {
+        return projCol42Sales;
+    }
+
+    public void setProjCol42Sales(String projCol42Sales) {
+        this.projCol42Sales = projCol42Sales;
+    }
+
+    public String getProjCol43Sales() {
+        return projCol43Sales;
+    }
+
+    public void setProjCol43Sales(String projCol43Sales) {
+        this.projCol43Sales = projCol43Sales;
+    }
+
+    public String getProjCol44Sales() {
+        return projCol44Sales;
+    }
+
+    public void setProjCol44Sales(String projCol44Sales) {
+        this.projCol44Sales = projCol44Sales;
+    }
+
+    public String getProjCol45Sales() {
+        return projCol45Sales;
+    }
+
+    public void setProjCol45Sales(String projCol45Sales) {
+        this.projCol45Sales = projCol45Sales;
+    }
+
+    public String getProjCol46Sales() {
+        return projCol46Sales;
+    }
+
+    public void setProjCol46Sales(String projCol46Sales) {
+        this.projCol46Sales = projCol46Sales;
+    }
+
+    public String getProjCol47Sales() {
+        return projCol47Sales;
+    }
+
+    public void setProjCol47Sales(String projCol47Sales) {
+        this.projCol47Sales = projCol47Sales;
+    }
+
+    public String getProjCol48Sales() {
+        return projCol48Sales;
+    }
+
+    public void setProjCol48Sales(String projCol48Sales) {
+        this.projCol48Sales = projCol48Sales;
+    }
+    
+}
