c7204d8fb2e1bc2559700ccf591d207f41510f6e
abhiramgiri <abhiram.giri@gmail.com>
2015-01-14 14:26:23 +0530

    Create Utils.java

---
+++ custom/src/main/java/org/asi/ui/custom/demo/util/Utils.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.asi.ui.custom.demo.util;
+
+import com.vaadin.server.ThemeResource;
+
+/**
+ *
+ * @author Abhiram
+ */
+public class Utils {
+    public static Object[] viscolumnless = {"projectionType", "allocationType",
+        "frequency"};
+    public static String[] visheaderless = {"projectionType", "allocationType",
+        "frequency"};
+    public static Object[] viscolumnmore = {"projCol1Sales", "projCol2Sales",
+        "projCol3Sales", "projCol4Sales", "projCol5Sales", "projCol6Sales",
+        "projCol7Sales", "projCol8Sales", "projCol9Sales",
+        "projCol10Sales", "projCol11Sales", "projCol12Sales",
+        "projCol13Sales", "projCol14Sales", "projCol15Sales",
+        "projCol16Sales", "projCol17Sales", "projCol18Sales",
+        "projCol19Sales", "projCol20Sales", "projCol21Sales",
+        "projCol22Sales", "projCol23Sales", "projCol24Sales",
+        "projCol25Sales", "projCol26Sales", "projCol27Sales",
+        "projCol28Sales", "projCol29Sales", "projCol30Sales",
+        "projCol31Sales", "projCol32Sales", "projCol33Sales",
+        "projCol34Sales", "projCol35Sales", "projCol36Sales",
+        "projCol37Sales", "projCol38Sales", "projCol39Sales",
+        "projCol40Sales", "projCol41Sales", "projCol42Sales",
+        "projCol43Sales", "projCol44Sales", "projCol45Sales",
+        "projCol46Sales", "projCol47Sales", "projCol48Sales"};
+    
+    public static String[] visheadermore = {"projCol1Sales", "projCol2Sales",
+        "projCol3Sales", "projCol4Sales", "projCol5Sales", "projCol6Sales",
+        "projCol7Sales", "projCol8Sales", "projCol9Sales",
+        "projCol10Sales", "projCol11Sales", "projCol12Sales",
+        "projCol13Sales", "projCol14Sales", "projCol15Sales",
+        "projCol16Sales", "projCol17Sales", "projCol18Sales",
+        "projCol19Sales", "projCol20Sales", "projCol21Sales",
+        "projCol22Sales", "projCol23Sales", "projCol24Sales",
+        "projCol25Sales", "projCol26Sales", "projCol27Sales",
+        "projCol28Sales", "projCol29Sales", "projCol30Sales",
+        "projCol31Sales", "projCol32Sales", "projCol33Sales",
+        "projCol34Sales", "projCol35Sales", "projCol36Sales",
+        "projCol37Sales", "projCol38Sales", "projCol39Sales",
+        "projCol40Sales", "projCol41Sales", "projCol42Sales",
+        "projCol43Sales", "projCol44Sales", "projCol45Sales",
+        "projCol46Sales", "projCol47Sales", "projCol48Sales"};
+    
+    public static Object[] dviscolumnless = {"projectionType", "allocationType"};
+    public static String[] dvisheaderless = {"projectionType", "allocationType"};
+    public static Object[] dviscolumnmore = {"projCol1Sales", "projCol2Sales",
+        "projCol3Sales", "projCol4Sales", "projCol5Sales", "projCol6Sales",
+        "projCol7Sales", "projCol8Sales", "projCol9Sales",
+        "projCol10Sales"};
+    public static String[] dvisheadermore = {"projCol1Sales", "projCol2Sales",
+        "projCol3Sales", "projCol4Sales", "projCol5Sales", "projCol6Sales",
+        "projCol7Sales", "projCol8Sales", "projCol9Sales",
+        "projCol10Sales"};
+    
+    public static Object[] tviscolumnless = {"projectionType"};
+    public static String[] tvisheaderless = {"projectionType"};
+    public static Object[] tviscolumnmore = {"projCol1Sales", "projCol2Sales",
+        "projCol3Sales", "projCol4Sales"};
+    public static String[] tvisheadermore = {"projCol1Sales", "projCol2Sales",
+        "projCol3Sales", "projCol4Sales"};
+    
+    
+    public static final ThemeResource logoimg = new ThemeResource("images/logo.png");
+    public static final float maxSplitPosition = 1000;
+    public static final float minSplitPosition = 200;
+    public static final float splitPosition = 300;
+}
